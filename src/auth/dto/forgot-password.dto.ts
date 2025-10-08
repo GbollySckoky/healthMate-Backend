30166aa5764cb167dae7aba5a9f47aa042e5ab86
DYung26 <theadstore1407@gmail.com>
2025-10-08 22:13:44 +0100

    feat(auth): add `ForgotPasswordDto` with email validation

---
+++ src/auth/dto/forgot-password.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail } from "class-validator";
+
+export class ForgotPasswordDto {
+  @ApiProperty({ example: 'user@example.com', description: 'Email address to verify' })
+  @IsEmail()
+  email: string;
+}
+
